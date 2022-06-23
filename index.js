de8ca55776b805e8dda1203dfca8256979225d01
WassCodeur <wachioubouraima56@gmail.com>
2022-06-23 10:05:59 +0000

    my work after localstorage

--- index.js
+++ index.js
@@ -17,9 +17,36 @@
 // let UseList =[user1, user2]
 
 // console.log(UseList);
+
+//Affichage de la date et l'heure;
 let dates =document.getElementById('date')
 var date = new Date();
-let time = date.getHours() + ":" + ('0'+date.getMinutes()).slice(-2) + ":" + ('0'+date.getSeconds()).slice(-2); 
+let time = ('0'+date.getHours()).slice(-2) + ":" + ('0'+date.getMinutes()).slice(-2) + ":" + ('0'+date.getSeconds()).slice(-2); 
 var options = {weekday: "long", year: "numeric", month: "long", day: "2-digit"};
-// let diplayDate =(`${date.toLocaleDateString("fr-FR", options)} ${time}`);
 dates.innerHTML= (`${date.toLocaleDateString("fr-FR", options)} ${time}`)
+
+//Boites modales
+let Modal1 =document.getElementById('modal')
+let profile_btn=document.getElementsByClassName('profile')[0]
+let close_btn1 =document.getElementsByClassName('close')[0];
+profile_btn.addEventListener('click', displayModal1);
+close_btn1.addEventListener('click', displayModal1);
+function displayModal1(){
+    if(Modal1.style.visibility =='hidden'){
+        Modal1.style.visibility ='visible'
+    }else{
+        Modal1.style.visibility ='hidden'
+    }}
+
+    let Modal2 =document.getElementById('modal2');
+    let add_btn=document.getElementsByClassName('add')[0];
+    let close_btn2 =document.getElementsByClassName('close')[1];
+    add_btn.addEventListener('click', displayModal2);
+    close_btn2.addEventListener('click', displayModal2);
+    function displayModal2(){
+        if(Modal2.style.visibility =='hidden'){
+            Modal2.style.visibility ='visible'
+        }else{
+            Modal2.style.visibility ='hidden'
+        }};
+
